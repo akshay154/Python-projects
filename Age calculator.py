354e1efe52926ef9492a7d492e6e362b0528a7d1
akshay154 <70559532+akshay154@users.noreply.github.com>
2021-06-22 09:26:25 +0530

    added comments

--- Age calculator.py
+++ Age calculator.py
@@ -1,8 +1,17 @@
+#Take users name as input
 name = input("Enter your name :\n")
+
+#take users birth year as input and convert it into an integer
 year =int (input("Year of birth:\n"))
+
+#calculate age and convert it into string
 age =str(int(2020 - year))
+
+#printing the calculated age of user
 print("========================================")
 print("Hello "+name+ "!" + " you are now "+ age + " years old ||" )
 print("========================================")
+
+#takes input to exit 
 input("press enter to quit")
 
